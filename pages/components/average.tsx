d43fa36c2a9c3791b11c265d4ef46943ba66cd23
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-02-15 09:40:06 -0300

    solution

--- pages/components/average.tsx
+++ pages/components/average.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import { TableRow, Button } from "@mui/material";
-import Paper from "@mui/material/Paper";
 import { useEffect, useState } from "react";
-
 import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Button,
+  Paper
+} from "@mui/material";
+
 interface Student {
   id: string;
   name: string;
--- pages/components/createStudent.tsx
+++ pages/components/createStudent.tsx
@@ -1,168 +1,191 @@
 import React, { useState } from 'react'
 import { useForm,Controller } from 'react-hook-form'
-import { TextField, Typography,Box } from "@mui/material";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { width } from '@mui/system';
-import { alignProperty } from '@mui/material/styles/cssUtils';
+import { TextField, Typography, Box, Button } from "@mui/material";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import Link from "next/link";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
-const NameRules = {
-  required: "Please, the field name is required",
-  minLength: {value:2, message: "Please, the field name should have at least 2 chars"}
-};
-
-const CourseRules = {
-  required: "Please, the field course is required",
-  minLength: {
-    value: 2,
-    message: "Please, the field course should have at least 2 chars",
-  },
-};
-
-const EmailRules = {
-  required: "Please, the field course is required",
-  minLength: {
-    value: 2,
-    message: "Please, the field course should have at least 2 chars",
-  },
-  pattern: {
-    value: /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i,
-    message: "Invalid email, please enter a valid format"
-  },
-};
-
-type StudentData = {
+interface Student {
   name: string,
-  course:string,
-  email:string,
-  birthdate?: string,
-  average:{
-    firstQuarter:string,
-    secondQuarter:string
-  }
-  errors:"null "
-};
+  course: string,
+  firstQuarter: string,
+  secondQuarter:string
+}
+
+const loginSchema = yup
+  .object({
+    name: yup
+      .string()
+      .required("Please, the field name is required")
+      .min(2, "Please, the field name should have at least 2 chars"),
+    course: yup
+      .string()
+      .required("Please, the field course is required")
+      .min(2, "Please, the field course should have at least 2 chars"),
+    email: yup
+      .string()
+      .required("Please, the field email is required")
+      .email("Invalid mail"),
+    firstQuarter: yup
+      .number()
+      .required("Please, the field firstQuarter is required"),
+    secondQuarter: yup
+      .number()
+      .required("Please, the field firstQuarter is required")
+  })
+  .required();
+
 
 export const Form: React.FC = () => {
-  const { register,control, handleSubmit,setFocus, formState:{errors} } = useForm()
-const [selectedDate, handleDateChange] = useState(new Date());
+  const [studentData, setStudentData] = useState <Student>();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(loginSchema) });
+
   function onSubmit(data: any) {
-    console.log(data)
+    fetch('/api/create', {method: "POST",headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(data)
+    })
+    .then(() => setStudentData(data))
+    .catch(error => console.error(error.message));
   }
 
-  React.useEffect(()=>{
-    setFocus("user")
-  },[])
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Box>
-        <Typography
-          variant="h3"
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          Create a new student
-        </Typography>
-        {/* INPUT NAME */}
-        <Controller
-          name="name"
-          control={control}
-          rules={NameRules}
-          defaultValue=""
-          render={({ field: { onChange, value, ref } }) => (
-            <TextField
-              label={"Name:"}
-              fullWidth
-              inputRef={ref}
-              onChange={onChange}
-              value={value}
-              error={!!errors.name}
-              helperText={`${errors.name?.message || ""}`}
-            />
-          )}
-        />
-        {/* INPUT COURSE */}
-        <Controller
-          name="course"
-          control={control}
-          rules={CourseRules}
-          defaultValue=""
-          render={({ field: { onChange, value, ref } }) => (
-            <TextField
-              label={"Course:"}
-              fullWidth
-              inputRef={ref}
-              onChange={onChange}
-              value={value}
-              error={!!errors.name}
-              helperText={`${errors.course?.message || ""}`}
-            />
-          )}
-        />
-        {/* INPUT EMAIL */}
-        <Controller
-          name="mail"
-          control={control}
-          rules={EmailRules}
-          defaultValue=""
-          render={({ field: { onChange, value, ref } }) => (
-            <TextField
-              label={"Email:"}
-              fullWidth
-              inputRef={ref}
-              onChange={onChange}
-              value={value}
-              error={!!errors.name}
-              helperText={`${errors.mail?.message || ""}`}
-            />
-          )}
-        />
-        <Typography variant="subtitle1" component="h2">
-          Promedio:
-        </Typography>
-        {/* INPUT FIRST QUARTER */}
-        <Controller
-          name="firstQuarter"
-          control={control}
-          defaultValue=""
-          render={({ field: { onChange, value, ref } }) => (
-            <TextField
-              sx={{ width: "50%" }}
-              label={"First Quarter:"}
-              inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-              fullWidth
-              inputRef={ref}
-              type="number"
-              onChange={onChange}
-              value={value}
-            />
-          )}
-        />
-        {/* INPUT SECOND QUARTER */}
-        <Controller
-          name="secondQuarter"
-          control={control}
-          defaultValue=""
-          render={({ field: { onChange, value, ref } }) => (
-            <TextField
-              sx={{ width: "50%" }}
-              label={"Second Quarter:"}
-              inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-              fullWidth
-              inputRef={ref}
-              type="number"
-              onChange={onChange}
-              value={value}
-            />
-          )}
-        />
-      </Box>
-    </form>
+    <>
+      <Link href="/">
+        <Button variant="text">Show All</Button>
+      </Link>
+      <Link href="/average">
+        <Button variant="text">
+          filter by average <ArrowDownwardIcon />
+        </Button>
+      </Link>
+      <form>
+        <Box>
+          <Typography
+            variant="h3"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            Create a new student
+          </Typography>
+          {/* INPUT NAME */}
+          <Controller
+            name="name"
+            control={control}
+            defaultValue=""
+            render={({ field: { onChange, value, ref } }) => (
+              <TextField
+                label={"Name:"}
+                fullWidth
+                inputRef={ref}
+                onChange={onChange}
+                value={value}
+                error={!!errors.name}
+                helperText={`${errors.name?.message || ""}`}
+              />
+            )}
+          />
+          {/* INPUT COURSE */}
+          <Controller
+            name="course"
+            control={control}
+            defaultValue=""
+            render={({ field: { onChange, value, ref } }) => (
+              <TextField
+                label={"Course:"}
+                fullWidth
+                inputRef={ref}
+                onChange={onChange}
+                value={value}
+                error={!!errors.name}
+                helperText={`${errors.course?.message || ""}`}
+              />
+            )}
+          />
+          {/* INPUT EMAIL */}
+          <Controller
+            name="email"
+            control={control}
+            defaultValue=""
+            render={({ field: { onChange, value, ref } }) => (
+              <TextField
+                label={"Email:"}
+                fullWidth
+                inputRef={ref}
+                onChange={onChange}
+                value={value}
+                error={!!errors.email}
+                helperText={`${errors.email?.message || ""}`}
+              />
+            )}
+          />
+          <Typography variant="subtitle1" component="h2">
+            Average
+          </Typography>
+          {/* INPUT FIRST QUARTER */}
+          <Controller
+            name="firstQuarter"
+            control={control}
+            defaultValue={0}
+            render={({ field: { onChange, value, ref } }) => (
+              <TextField
+                sx={{ width: "50%" }}
+                label={"First Quarter:"}
+                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                fullWidth
+                inputRef={ref}
+                type="number"
+                onChange={onChange}
+                value={value}
+                error={!!errors.firstQuarter}
+                helperText={`${errors.firstQuarter?.message || ""}`}
+              />
+            )}
+          />
+          {/* INPUT SECOND QUARTER */}
+          <Controller
+            name="secondQuarter"
+            control={control}
+            defaultValue={0}
+            render={({ field: { onChange, value, ref } }) => (
+              <TextField
+                sx={{ width: "50%" }}
+                label={"Second Quarter:"}
+                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                fullWidth
+                inputRef={ref}
+                type="number"
+                onChange={onChange}
+                value={value}
+                error={!!errors.secondQuarter}
+                helperText={`${errors.secondQuarter?.message || ""}`}
+              />
+            )}
+          />
+          <Button variant="text" onClick={handleSubmit(onSubmit)}>
+            Crear
+          </Button>
+        </Box>
+      </form>
+      {studentData?.name && (
+        <Box>
+          <Typography variant="h3">Saved!</Typography>
+          <h3>Student Name: {studentData?.name}</h3>
+          <p>Course: {studentData?.course}</p>
+          <h4>Average: </h4>
+          <p>FirstQuarter : {studentData?.firstQuarter}</p>
+          <p>Second Quarter: {studentData?.secondQuarter}</p>
+        </Box>
+      )}
+    </>
   );
 
 }
